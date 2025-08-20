21be443a9e97b36ab178bb7688d7c922babede41
Khajani Sivapala <khajani.sivapala@gmail.com>
2025-08-19 23:18:35 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,7 +13,8 @@ toggleBtn.addEventListener('click', () => {
 });
 
 // Highlight sidebar nav on scroll
-const links = document.querySelectorAll('aside nav ul li a');
+const links = document.querySelectorAll('.sidebar nav ul li a');
+
 const sections = Array.from(links).map(link => document.querySelector(link.getAttribute('href')));
 window.addEventListener('scroll', () => {
   const scrollPos = window.scrollY + window.innerHeight / 3;
